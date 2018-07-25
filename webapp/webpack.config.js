59bf02da2c620578028959db57899f0530b191d4
Sy Le <syle@linkedin.com>
2018-07-25 14:15:43 -0700

    loading font...

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
         }, {
             test: /\.(png|svg|jpg|gif)$/,
             use: ['file-loader']
+        }, {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: ['file-loader']
         }]
     }
 };
